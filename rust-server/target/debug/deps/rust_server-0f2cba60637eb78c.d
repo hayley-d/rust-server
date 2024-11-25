51d94b4ffa47766cac6c69dd6d6b1a058c612e44
hayley-d <hayleydod@proton.me>
2024-11-25 14:51:44 +0200

    removed build files

--- rust-server/target/debug/deps/rust_server-0f2cba60637eb78c.d
+++
@@ -1,5 +0,0 @@
-/home/hayley/Documents/rust-server/rust-server/target/debug/deps/librust_server-0f2cba60637eb78c.rmeta: src/main.rs
-
-/home/hayley/Documents/rust-server/rust-server/target/debug/deps/rust_server-0f2cba60637eb78c.d: src/main.rs
-
-src/main.rs:
--- rust-server/target/debug/deps/rust_server-3453ef4a5eadb748.d
+++
@@ -1,5 +0,0 @@
-/home/hayley/Documents/rust-server/rust-server/target/debug/deps/librust_server-3453ef4a5eadb748.rmeta: src/main.rs
-
-/home/hayley/Documents/rust-server/rust-server/target/debug/deps/rust_server-3453ef4a5eadb748.d: src/main.rs
-
-src/main.rs:
