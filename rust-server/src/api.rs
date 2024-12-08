996bcbcdde6fda2ad69c13a74d6203bc55ddbf4e
hayley-d <hayleydod@proton.me>
2024-12-08 16:30:33 +0200

    added login route

--- rust-server/src/api.rs
+++ rust-server/src/api.rs
@@ -1,5 +1,6 @@
 use crate::{ContentType, ErrorType, HttpCode, HttpMethod, MyDefault, Request, Response};
 use argon2::password_hash::SaltString;
+use argon2::PasswordHash;
 use argon2::{password_hash::Salt, Argon2, PasswordHasher, PasswordVerifier};
 use rand::rngs::OsRng;
 use rand::Rng;
@@ -107,6 +108,86 @@ async fn handle_post(request: Request) -> Response {
         );
 
         return response;
+    } else if request.uri == "/login" {
+        let user: HashMap<String, String> = match serde_json::from_str(&request.body) {
+            Ok(u) => u,
+            Err(_) => {
+                eprintln!("Error parsing json");
+                return response.code(HttpCode::BadRequest);
+            }
+        };
+
+        let input_username: &str = &user["username"];
+        let input_password: &str = &user["password"];
+
+        /*let cookie_header: Vec<String> = request
+            .headers
+            .into_iter()
+            .filter(|h| h.contains("Cookie: session="))
+            .collect();
+
+        let cookie_header = match cookie_header.get(0) {
+            Some(h) => h,
+            None => {
+                eprintln!("Error parsing cookie header");
+                return response.code(HttpCode::BadRequest);
+            }
+        };
+
+        let header_parts: Vec<&str> = cookie_header.split_whitespace().collect();
+
+        let cookie_value: &str = match header_parts.get(1) {
+            Some(v) => v,
+            None => {
+                eprintln!("Error parsing cookie header");
+                return response.code(HttpCode::BadRequest);
+            }
+        };
+
+        if verify_cookie(cookie_value).await {*/
+        let contents: String = fs::read_to_string("static/users.txt").await.unwrap();
+
+        let user_values: String = match contents
+            .lines()
+            .filter(|l| l.contains(input_username))
+            .collect::<Vec<&str>>()
+            .get(0)
+        {
+            Some(l) => l.to_string(),
+            None => {
+                eprintln!("User does not exist");
+                return response.code(HttpCode::BadRequest);
+            }
+        };
+
+        let user_values: Vec<&str> = user_values.split('|').collect();
+
+        if user_values.len() != 3 {
+            eprintln!("User does not exist");
+            return response.code(HttpCode::BadRequest);
+        }
+
+        if user_values[0] == input_username {
+            match validate_password(input_password, user_values[1]) {
+                Ok(v) if v == true => (),
+                Ok(_) => {
+                    eprintln!("Incorrect Password");
+                    return response.code(HttpCode::BadRequest);
+                }
+                Err(_) => {
+                    eprintln!("Error validating password");
+                    return response.code(HttpCode::InternalServerError);
+                }
+            }
+
+            response.add_header(
+                String::from("Set-Cookie"),
+                format!("session={}; HttpOnly", user_values[2]),
+            );
+            return response;
+        }
+
+        //}
     }
 
     return response.code(HttpCode::InternalServerError);
@@ -180,6 +261,27 @@ async fn insert_user(username: String, password: String, session: String) -> Res
     Ok(())
 }
 
+fn validate_password(password: &str, hashed_password: &str) -> Result<bool, ErrorType> {
+    let argon2 = Argon2::default();
+
+    // Parse the hashed password
+    let parsed_hash = PasswordHash::new(hashed_password).map_err(|e| {
+        eprintln!("Error parsing hashed password: {:?}", e);
+        ErrorType::InternalServerError(String::from(
+            "Problem occurred when validating the password",
+        ))
+    })?;
+
+    // Verify the password against the hashed password
+    match argon2.verify_password(password.as_bytes(), &parsed_hash) {
+        Ok(_) => Ok(true),
+        Err(_) => {
+            eprintln!("Incorrect password");
+            return Err(ErrorType::BadRequest(String::from("Incorrect Password")));
+        }
+    }
+}
+
 fn generate_session_id() -> String {
     let mut rng = rand::thread_rng();
     (0..32)
@@ -227,6 +329,25 @@ mod tests {
             body: request_body,
             method: HttpMethod::POST,
             uri: "/signup".to_string(),
+        };
+
+        let response: Response = handle_post(request).await;
+        assert_eq!(response.code, HttpCode::Ok);
+    }
+
+    #[tokio::test]
+    async fn test_login() {
+        let request_body = json!({
+            "username": "hayley",
+            "password": "password"
+        })
+        .to_string();
+
+        let request = Request {
+            headers: Vec::new(),
+            body: request_body,
+            method: HttpMethod::POST,
+            uri: "/login".to_string(),
         };
 
         let response: Response = handle_post(request).await;
